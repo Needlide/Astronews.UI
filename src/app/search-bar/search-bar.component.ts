831551ffe106523de271f6af7eb751df18f438a6
Needlide <max.litvinyuck553@gmail.com>
2024-12-06 21:21:51 +0200

    Removed unnecessary logic for emitting value to the parent component

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { SearchService } from '../search.service';
 import { Subject, debounceTime } from 'rxjs';
 import { FormsModule } from '@angular/forms';
@@ -8,29 +8,27 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.scss'
+  styleUrl: './search-bar.component.scss',
 })
 export class SearchBarComponent implements OnInit {
   public searchTerm: string = '';
   private searchSubject = new Subject<string>();
 
   @Input() debounceTimeDuration: number = 730;
-  // Notify the parent components about changes in search term
-  @Output() searchChange = new EventEmitter<string>();
 
   constructor(private searchService: SearchService) {}
 
   ngOnInit(): void {
-    this.searchSubject.pipe(debounceTime(this.debounceTimeDuration)).subscribe((searchText) => {
-      this.searchService.setSearchTerm(searchText);
-      this.searchChange.emit(searchText); // Emit the search term change to a parent
-    });
+    this.searchSubject
+      .pipe(debounceTime(this.debounceTimeDuration))
+      .subscribe((searchText) => {
+        this.searchService.setSearchTerm(searchText);
+      });
   }
 
   clearSearch(): void {
     this.searchService.setSearchTerm('');
     this.searchTerm = '';
-    this.searchChange.emit('');
   }
 
   onSearchTermChange(): void {

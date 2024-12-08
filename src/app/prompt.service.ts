8a4041d46c44be162b7943389cd47541428095be
Needlide <max.litvinyuck553@gmail.com>
2024-12-09 00:40:33 +0200

    Added comments for readability. Added property for gallery

--- src/app/prompt.service.ts
+++ src/app/prompt.service.ts
@@ -1,43 +1,16 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PromptService {
+  // LIBRARY
   private _libraryNext: string = '';
   public get LibraryNext(): string {
     return this._libraryNext;
   }
   public set LibraryNext(value: string) {
     this._libraryNext = value;
-  }
-  private _newsNext: string = '';
-  public get NewsNext(): string {
-    return this._newsNext;
-  }
-  public set NewsNext(value: string) {
-    this._newsNext = value;
-  }
-  private _marsCuriosityNext: string = '';
-  public get MarsCuriosityNext(): string {
-    return this._marsCuriosityNext;
-  }
-  public set MarsCuriosityNext(value: string) {
-    this._marsCuriosityNext = value;
-  }
-  private _marsOpportunityNext: string = '';
-  public get MarsOpportunityNext(): string {
-    return this._marsOpportunityNext;
-  }
-  public set MarsOpportunityNext(value: string) {
-    this._marsOpportunityNext = value;
-  }
-  private _marsSpiritNext: string = '';
-  public get MarsSpiritNext(): string {
-    return this._marsSpiritNext;
-  }
-  public set MarsSpiritNext(value: string) {
-    this._marsSpiritNext = value;
   }
 
   private _libraryPrev: string = '';
@@ -47,6 +20,25 @@ export class PromptService {
   public set LibraryPrev(value: string) {
     this._libraryPrev = value;
   }
+
+  private _isDataAvailable: boolean = false;
+  public get isDataAvailable(): boolean {
+    return this._isDataAvailable;
+  }
+  public set isDataAvailable(value: boolean) {
+    this._isDataAvailable = value;
+  }
+  // ---------------------------
+
+  // NEWS
+  private _newsNext: string = '';
+  public get NewsNext(): string {
+    return this._newsNext;
+  }
+  public set NewsNext(value: string) {
+    this._newsNext = value;
+  }
+
   private _newsPrev: string = '';
   public get NewsPrev(): string {
     return this._newsPrev;
@@ -54,6 +46,17 @@ export class PromptService {
   public set NewsPrev(value: string) {
     this._newsPrev = value;
   }
+  // ----------------------------
+
+  // MARS CURIOSITY
+  private _marsCuriosityNext: string = '';
+  public get MarsCuriosityNext(): string {
+    return this._marsCuriosityNext;
+  }
+  public set MarsCuriosityNext(value: string) {
+    this._marsCuriosityNext = value;
+  }
+
   private _marsCuriosityPrev: string = '';
   public get MarsCuriosityPrev(): string {
     return this._marsCuriosityPrev;
@@ -61,6 +64,17 @@ export class PromptService {
   public set MarsCuriosityPrev(value: string) {
     this._marsCuriosityPrev = value;
   }
+  // ----------------------------
+
+  // MARS OPPORTUNITY
+  private _marsOpportunityNext: string = '';
+  public get MarsOpportunityNext(): string {
+    return this._marsOpportunityNext;
+  }
+  public set MarsOpportunityNext(value: string) {
+    this._marsOpportunityNext = value;
+  }
+
   private _marsOpportunityPrev: string = '';
   public get MarsOpportunityPrev(): string {
     return this._marsOpportunityPrev;
@@ -68,6 +82,17 @@ export class PromptService {
   public set MarsOpportunityPrev(value: string) {
     this._marsOpportunityPrev = value;
   }
+  // -----------------------------
+
+  // MARS SPIRIT
+  private _marsSpiritNext: string = '';
+  public get MarsSpiritNext(): string {
+    return this._marsSpiritNext;
+  }
+  public set MarsSpiritNext(value: string) {
+    this._marsSpiritNext = value;
+  }
+
   private _marsSpiritPrev: string = '';
   public get MarsSpiritPrev(): string {
     return this._marsSpiritPrev;
@@ -75,13 +100,7 @@ export class PromptService {
   public set MarsSpiritPrev(value: string) {
     this._marsSpiritPrev = value;
   }
-  private _apodPrev: string = '';
-  public get ApodPrev(): string {
-    return this._apodPrev;
-  }
-  public set ApodPrev(value: string) {
-    this._apodPrev = value;
-  }
+  // ------------------------------
 
-  constructor() { }
+  constructor() {}
 }

84a48e7a4550cf88cbb0eafe0c4c1f78bf79d19b
Needlide <max.litvinyuck553@gmail.com>
2024-12-09 00:46:51 +0200

    Returned search service to previous state. Every search logic is tightly coupled with its page, so no centralized, universal search logic

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { CachingService } from '../caching.service';
-import { parseSearchTerm } from '../search.util';
-import { DataService } from '../data.service';
-import { parse } from 'path';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -12,10 +7,7 @@ export class SearchService {
   private _searchTerm = new BehaviorSubject<string>('');
   searchTerm$ = this._searchTerm.asObservable();
 
-  constructor(
-    private cacheService: CachingService,
-    private dataService: DataService
-  ) {}
+  constructor() {}
 
   setSearchTerm(term: string) {
     this._searchTerm.next(term);
@@ -23,21 +15,5 @@ export class SearchService {
 
   getSearchTerm(): string {
     return this._searchTerm.getValue();
-  }
-
-  performSearch(page_key: string, data: any[]): any[] {
-    let term = this.getSearchTerm();
-
-    // start searching from 3 symbols
-    if (!term || term.length < 4) {
-      return [];
-    }
-
-    // retrieve cache by term key
-    let cache = this.cacheService.get(page_key, term);
-
-    if (cache) {
-      return cache;
-    }
   }
 }

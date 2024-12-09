1e86ff88f70331b7c49701731265f3b8c219cebc
Needlide <max.litvinyuck553@gmail.com>
2024-12-09 20:42:22 +0200

    Added constants for error url and url for mars curiosity

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -2,3 +2,5 @@ export const minSymbolsToTriggerSearch = 4;
 export const errorMessageDataFetch =
   'Error occured during data fetch. Please, try again shortly.';
 export const errorUrlGallery = '/NasaGallery';
+export const errorUrlMarsCuriosity = '/MarsPhotos/Curiosity';
+export const errorUrl = '/Error';

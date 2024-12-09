8880247806e49ef5a855afcc2306d4895e2aca39
Needlide <max.litvinyuck553@gmail.com>
2024-12-09 22:05:36 +0200

    Added error url for news page

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -3,4 +3,5 @@ export const errorMessageDataFetch =
   'Error occured during data fetch. Please, try again shortly.';
 export const errorUrlGallery = '/NasaGallery';
 export const errorUrlMarsCuriosity = '/MarsPhotos/Curiosity';
+export const errorUrlNews = '/News';
 export const errorUrl = '/Error';

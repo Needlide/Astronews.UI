e1dfffd636b022b22edd095123e7651b94e9e5fd
Needlide <max.litvinyuck553@gmail.com>
2024-12-10 21:55:46 +0200

    Added caching the data after successfull API call

--- src/app/news/news.effects.ts
+++ src/app/news/news.effects.ts
@@ -1,27 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { DataService } from '../data.service';
-import { loadNews, loadNewsSuccess, loadNewsFailure } from './news.actions';
-import { catchError, map, mergeMap } from 'rxjs/operators';
+import { NewsActions } from './news.actions';
+import { catchError, map, mergeMap, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { CachingService } from '../cache/caching.service';
+import { DEFAULT_CACHE_KEYS, PAGE_KEYS } from '../cache/cache-keys';
+import { NewsCache } from '../models/news/news-cache-model';
 
 @Injectable()
 export class NewsEffects {
-  constructor(private actions$: Actions, private dataService: DataService) {}
+  constructor(
+    private actions$: Actions,
+    private dataService: DataService,
+    private cacheService: CachingService
+  ) {}
 
   loadNews$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(loadNews),
+      ofType(NewsActions.loadData),
       mergeMap((action) =>
         this.dataService.getNews(action.url).pipe(
-          map((response) => loadNewsSuccess({
-            data: response.results,
-            nextUrl: response.next,
-          })),
-          catchError((error) => of(loadNewsFailure({ error: error.message })))
+          tap((response) => {
+            const newsCache: NewsCache = {
+              nextUrl: response.next,
+              prevUrl: response.previous,
+              data: response.results,
+            };
+
+            this.cacheService.set(
+              PAGE_KEYS.NEWS,
+              DEFAULT_CACHE_KEYS.NEWS,
+              newsCache
+            );
+          }),
+          map((response) =>
+            NewsActions.loadDataSuccess({
+              data: response.results,
+              nextUrl: response.next,
+              prevUrl: response.previous,
+            })
+          ),
+          catchError((error) =>
+            of(NewsActions.loadDataFailure({ error: error.message }))
+          )
         )
       )
     )
   );
 }
-

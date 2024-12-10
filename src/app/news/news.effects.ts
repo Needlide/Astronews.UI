b32d4e8a5af7bf29d0ed8c20cf6cbe0f237e4f67
Needlide <max.litvinyuck553@gmail.com>
2024-12-10 22:16:18 +0200

    If cache exists, return it without making API call

--- src/app/news/news.effects.ts
+++ src/app/news/news.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { DataService } from '../data.service';
 import { NewsActions } from './news.actions';
-import { catchError, map, mergeMap, tap } from 'rxjs/operators';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { CachingService } from '../cache/caching.service';
 import { DEFAULT_CACHE_KEYS, PAGE_KEYS } from '../cache/cache-keys';
@@ -19,33 +19,50 @@ export class NewsEffects {
   loadNews$ = createEffect(() =>
     this.actions$.pipe(
       ofType(NewsActions.loadData),
-      mergeMap((action) =>
-        this.dataService.getNews(action.url).pipe(
-          tap((response) => {
-            const newsCache: NewsCache = {
-              nextUrl: response.next,
-              prevUrl: response.previous,
-              data: response.results,
-            };
+      switchMap((action) => {
+        // check if cache for the page exists
+        const cache = this.cacheService.get(
+          PAGE_KEYS.NEWS,
+          DEFAULT_CACHE_KEYS.NEWS
+        ) as NewsCache;
 
-            this.cacheService.set(
-              PAGE_KEYS.NEWS,
-              DEFAULT_CACHE_KEYS.NEWS,
-              newsCache
-            );
-          }),
-          map((response) =>
+        if (cache) {
+          return of(
             NewsActions.loadDataSuccess({
-              data: response.results,
-              nextUrl: response.next,
-              prevUrl: response.previous,
+              data: cache.data,
+              nextUrl: cache.nextUrl,
+              prevUrl: cache.prevUrl,
             })
-          ),
-          catchError((error) =>
-            of(NewsActions.loadDataFailure({ error: error.message }))
-          )
-        )
-      )
+          );
+        } else {
+          return this.dataService.getNews(action.url).pipe(
+            tap((response) => {
+              // save the data in the cache
+              const newsCache: NewsCache = {
+                nextUrl: response.next,
+                prevUrl: response.previous,
+                data: response.results,
+              };
+
+              this.cacheService.set(
+                PAGE_KEYS.NEWS,
+                DEFAULT_CACHE_KEYS.NEWS,
+                newsCache
+              );
+            }),
+            map((response) =>
+              NewsActions.loadDataSuccess({
+                data: response.results,
+                nextUrl: response.next,
+                prevUrl: response.previous,
+              })
+            ),
+            catchError((error) =>
+              of(NewsActions.loadDataFailure({ error: error.message }))
+            )
+          );
+        }
+      })
     )
   );
 }

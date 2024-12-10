ec08fab50177e7791f0b6763251e83317b5661e2
Needlide <max.litvinyuck553@gmail.com>
2024-12-10 21:52:12 +0200

    Added property for previous URL

--- src/app/models/news/news-cache-model.ts
+++ src/app/models/news/news-cache-model.ts
@@ -1,6 +1,8 @@
-import { NewsModel } from "./news.model";
+import { NewsModel } from './news.model';
 
+// model used to store the news data in cache
 export interface NewsCache {
-    nextUrl: string,
-    data: NewsModel[]
+  nextUrl: string;
+  prevUrl: string;
+  data: NewsModel[];
 }

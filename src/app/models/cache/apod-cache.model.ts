39bf2eee12a059a304687c52da696dd07902b8db
Needlide <max.litvinyuck553@gmail.com>
2024-12-13 00:07:31 +0200

    Changed types of properties related to API calls from string to Date

--- src/app/models/cache/apod-cache.model.ts
+++ src/app/models/cache/apod-cache.model.ts
@@ -2,8 +2,8 @@ import { ApodModel } from '../apod/apod.model';
 
 export interface ApodCache {
   data: ApodModel[];
-  nextStartYear: string;
-  prevStartYear: string;
-  nextEndYear: string;
-  prevEndYear: string;
+  nextStartYear: Date;
+  prevStartYear: Date;
+  nextEndYear: Date;
+  prevEndYear: Date;
 }

d948abb6c27e9e9a9d6134dcbcf72d88594f06b4
Needlide <max.litvinyuck553@gmail.com>
2024-12-13 19:12:50 +0200

    Implemented actions for mars curiosity component

---
+++ src/app/mars-photos-curiosity/mars-photos-curiosity.actions.ts
@@ -0,0 +1,13 @@
+import { createActionGroup, props } from '@ngrx/store';
+import { MarsModel } from '../models/mars/mars.model';
+
+export const MarsCuriosityActions = createActionGroup({
+  source: 'Mars Curiosity Page',
+  events: {
+    'Load Data': props<{ url: string; cacheKey: string }>(),
+    'Load Data Success': props<{
+      data: MarsModel[];
+    }>(),
+    'Load Data Failure': props<{ error: string }>(),
+  },
+});

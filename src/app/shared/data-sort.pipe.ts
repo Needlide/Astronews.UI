66ae06966180350ad99269f598bd45756c267583
Needlide <max.litvinyuck553@gmail.com>
2024-12-15 00:37:18 +0200

    Added shallow copying so the pipe doesn't tried to change the mutable arrays

--- src/app/shared/data-sort.pipe.ts
+++ src/app/shared/data-sort.pipe.ts
@@ -1,20 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'dataSort'
+  name: 'dataSort',
 })
 export class DataSortPipe implements PipeTransform {
-
   transform(array: any[], field: string): any[] {
     if (!Array.isArray(array)) {
       return array;
     }
-    array.sort((a: any, b: any) => {
+
+    const sortedArray = [...array];
+
+    sortedArray.sort((a: any, b: any) => {
       const dateA = new Date(a[field]);
       const dateB = new Date(b[field]);
       return dateA < dateB ? 1 : -1;
     });
-    return array;
-  }
 
+    return sortedArray;
+  }
 }

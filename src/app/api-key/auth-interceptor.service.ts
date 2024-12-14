ef6e19ed9e5e852a69ed15e999dc73c9dbffae10
Needlide <max.litvinyuck553@gmail.com>
2024-12-14 22:41:58 +0200

    Fixed import path

--- src/app/api-key/auth-interceptor.service.ts
+++ src/app/api-key/auth-interceptor.service.ts
@@ -1,29 +1,35 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpInterceptor, HttpRequest, HttpHandler, HttpEvent
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiKeyService } from './api-key.service';
-import { environment } from '../environments/environment.prod';
+import { environment } from '@/environments/environment.prod';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private apiKeyService: ApiKeyService) {}
 
-  constructor(private apiKeyService: ApiKeyService) { }
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     const urlsRequiringApiKey = [
       environment.api.marsCuriosityEndpoint,
-      environment.api.apodEndpoint
+      environment.api.apodEndpoint,
     ];
 
-    if (urlsRequiringApiKey.some(url => req.urlWithParams.includes(url))) {
+    if (urlsRequiringApiKey.some((url) => req.urlWithParams.includes(url))) {
       if (req.method !== 'OPTIONS') {
-        req = req.clone({ headers: req.headers.set('X-API-KEY', this.apiKeyService.getApiKey()) });
+        req = req.clone({
+          headers: req.headers.set('X-API-KEY', this.apiKeyService.getApiKey()),
+        });
       }
-
     }
     return next.handle(req);
   }

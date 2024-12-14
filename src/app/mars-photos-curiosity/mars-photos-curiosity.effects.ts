073c75e129341232d27f041fe19fe0b7049b8b0f
Needlide <max.litvinyuck553@gmail.com>
2024-12-14 20:46:05 +0200

    Implemented effects

---
+++ src/app/mars-photos-curiosity/mars-photos-curiosity.effects.ts
@@ -0,0 +1,63 @@
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { DataService } from '../data.service';
+import { CachingService } from '../cache/caching.service';
+import { MarsCuriosityActions } from './mars-photos-curiosity.actions';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
+import { DEFAULT_CACHE_KEYS, PAGE_KEYS } from '../cache/cache-keys';
+import { MarsCuriosityCache } from '../models/cache/mars-curiosity.model';
+
+export class MarsCuriosityEffects {
+  constructor(
+    private actions$: Actions,
+    private dataService: DataService,
+    private cacheService: CachingService
+  ) {}
+
+  loadData$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(MarsCuriosityActions.loadData),
+      switchMap((action) => {
+        const cache = this.cacheService.get(
+          PAGE_KEYS.MARS_CURIOSITY,
+          DEFAULT_CACHE_KEYS.MARS_CURIOSITY
+        ) as MarsCuriosityCache;
+
+        if (cache) {
+          return of(
+            MarsCuriosityActions.loadDataSuccess({
+              data: cache.data,
+              maxSol: cache.maxSol,
+            })
+          );
+        } else {
+          return this.dataService.getMarsLatestPhotos(action.url).pipe(
+            tap((response) => {
+              const maxSol = response.latest_photos[0]?.rover?.max_sol ?? 0;
+
+              const curiosityCache: MarsCuriosityCache = {
+                data: response.latest_photos,
+                maxSol: maxSol,
+              };
+
+              this.cacheService.set(
+                PAGE_KEYS.MARS_CURIOSITY,
+                DEFAULT_CACHE_KEYS.MARS_CURIOSITY,
+                curiosityCache
+              );
+            }),
+            map((response) => {
+              const maxSol = response.latest_photos[0]?.rover?.max_sol ?? 0;
+              return MarsCuriosityActions.loadDataSuccess({
+                data: response.latest_photos,
+                maxSol: maxSol,
+              });
+            }),
+            catchError((error) =>
+              of(MarsCuriosityActions.loadDataFailure({ error: error.message }))
+            )
+          );
+        }
+      })
+    )
+  );
+}

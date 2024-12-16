ccde3b0dab71b694ca4700ff9cdbd539430f0203
Needlide <max.litvinyuck553@gmail.com>
2024-12-16 14:11:25 +0200

    Changed the format from `yyyy-MM-dd` to `MMMM d, yyyy` for readability

--- src/app/shared/date-format.pipe.ts
+++ src/app/shared/date-format.pipe.ts
@@ -2,17 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
 @Pipe({
-  name: 'dateFormat'
+  name: 'dateFormat',
 })
 export class DateFormatPipe implements PipeTransform {
-
   transform(value: string): string | null {
     if (value) {
       const date = new Date(value);
       const datePipe = new DatePipe('en-US');
-      return datePipe.transform(date, 'yyyy-MM-dd');
+      return datePipe.transform(date, 'MMMM d, yyyy');
     }
     return '';
   }
-
 }

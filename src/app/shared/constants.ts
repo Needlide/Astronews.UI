1ecf225322bbd7d8d8ca6fb3c0cede42ae48270c
Needlide <max.litvinyuck553@gmail.com>
2024-12-18 17:11:05 +0200

    Removed routes from constant

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,8 +1,4 @@
 export const minSymbolsToTriggerSearch = 4;
 export const errorMessageDataFetch =
   'Error occured during data fetch. Please, try again shortly.';
-export const errorUrlGallery = '/NasaGallery';
-export const errorUrlMarsCuriosity = '/MarsPhotos/Curiosity';
-export const errorUrlNews = '/News';
-export const errorUrl = '/Error';
 export const searchPause = 730;

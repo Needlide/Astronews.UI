11c3f6452a90b349aa8d5c6be3a2e3fdeb5e3af0
Needlide <max.litvinyuck553@gmail.com>
2024-12-26 14:14:23 +0200

    Added error message constant for APOD page

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,4 +1,6 @@
 export const minSymbolsToTriggerSearch = 4;
 export const errorMessageDataFetch =
   'Error occured during data fetch. Please, try again shortly.';
+export const errorMessageNoPropertyApod =
+  'Error occured during search. No such property. Possible options are: `t` - title, `e` - explanation, `c` - copyright, `d` - date.';
 export const searchPause = 730;

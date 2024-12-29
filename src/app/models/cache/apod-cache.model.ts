4d870c712c4f4403e8ed56bcf6ccf662d2e321f6
Needlide <max.litvinyuck553@gmail.com>
2024-12-29 20:49:07 +0200

    Updated model to correspond to new caching service

--- src/app/models/cache/apod-cache.model.ts
+++ src/app/models/cache/apod-cache.model.ts
@@ -2,10 +2,5 @@ import { ApodModel } from '../apod/apod.model';
 
 export interface ApodCache {
   data: ApodModel[];
-  paginationValues: ApodPayload[];
-}
-
-export interface ApodPayload {
-  startDate: Date;
-  endDate: Date;
+  expiry: number;
 }

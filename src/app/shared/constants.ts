1cbd4bfe6f07e96edb3d96e568248e8e2b509d17
Needlide <max.litvinyuck553@gmail.com>
2024-12-30 20:29:21 +0200

    Added message for cache retrieval error

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,6 +1,6 @@
 export const minSymbolsToTriggerSearch = 4;
 export const errorMessageDataFetch =
   'Error occured during data fetch. Please, try again shortly.';
-export const errorMessageNoPropertyApod =
-  'Error occured during search. No such property. Possible options are: `t` - title, `e` - explanation, `c` - copyright, `d` - date.';
+export const errorMessageCacheRetrieve =
+  'Error occured during cache retrieval. Try to refresh the page.';
 export const searchPause = 730;

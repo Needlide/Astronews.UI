6d2bbfa0f0f6397dc559e46ca35cc371e601097b
Needlide <max.litvinyuck553@gmail.com>
2024-12-30 20:50:07 +0200

    Added current page and total items to the state. Covered new actions

--- src/app/apod/apod.reducer.ts
+++ src/app/apod/apod.reducer.ts
@@ -6,12 +6,16 @@ export interface ApodState {
   data: ApodModel[];
   isLoading: boolean;
   error: string | null;
+  page: number;
+  totalItems: number;
 }
 
 export const initialState: ApodState = {
   data: [],
   isLoading: false,
   error: null,
+  page: 1,
+  totalItems: 0,
 };
 
 export const apodReducer = createReducer(
@@ -31,5 +35,46 @@ export const apodReducer = createReducer(
     ...state,
     isLoading: false,
     error,
+  })),
+  on(ApodActions.initiateSearch, (state) => ({
+    ...state,
+    isLoading: true,
+    error: null,
+  })),
+  on(ApodActions.initiateSearchSuccess, (state, { data }) => ({
+    ...state,
+    data: data,
+    isLoading: false,
+    error: null,
+  })),
+  on(ApodActions.initiateSearchFailure, (state, { error }) => ({
+    ...state,
+    isLoading: false,
+    error,
+  })),
+  on(ApodActions.loadPageFromCache, (state, { pageNumber }) => ({
+    ...state,
+    isLoading: true,
+    error: null,
+    page: pageNumber,
+  })),
+  on(ApodActions.loadPageFromCacheSuccess, (state, { data }) => ({
+    ...state,
+    data: data,
+    isLoading: false,
+    error: null,
+  })),
+  on(ApodActions.loadPageFromCacheFailure, (state, { error }) => ({
+    ...state,
+    isLoading: false,
+    error,
+  })),
+  on(ApodActions.calculateTotalItemsSuccess, (state, { totalItems }) => ({
+    ...state,
+    totalItems,
+  })),
+  on(ApodActions.changeCurrentPage, (state, { currentPage }) => ({
+    ...state,
+    page: currentPage,
   }))
 );

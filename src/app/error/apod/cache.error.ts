b4622eb7b96ca0c888d6ac18011c73a05cd801f3
Needlide <max.litvinyuck553@gmail.com>
2025-01-01 20:55:37 +0200

    Removed custom error types. Switched to Either type

--- src/app/error/apod/cache.error.ts
+++
@@ -1,6 +0,0 @@
-export class CacheError extends Error {
-  constructor(message: string, public pageNumber?: number) {
-    super(message);
-    this.name = 'CacheError';
-  }
-}
--- src/app/error/apod/search.error.ts
+++
@@ -1,6 +0,0 @@
-export class SearchError extends Error {
-  constructor(message: string, public invalidPrefix?: string) {
-    super(message);
-    this.name = 'SearchError';
-  }
-}

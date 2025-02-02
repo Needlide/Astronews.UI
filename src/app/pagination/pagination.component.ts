08ad59a92cc23ca6144517c13b5dc3603fcd52e6
Needlide <max.litvinyuck553@gmail.com>
2025-02-02 21:41:11 +0200

    feature: add input and output for itemsPerPage, improve total pages calculation with safety check Removed NgFor from dependencies, added input for selectedItemsPerPage and output for itemsPerPageChanged. Implemented method for notifying about itemsPerPage change

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,11 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [NgFor, FormsModule],
+  imports: [FormsModule],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss',
 })
@@ -13,15 +12,18 @@ export class PaginationComponent {
   @Input() displayItemsPerPage: boolean = true;
   @Input() totalItems: number = 0;
   @Input() currentPage: number = 1;
+  @Input() selectedItemsPerPage: number = 50;
   @Output() pageChanged: EventEmitter<number> = new EventEmitter();
+  @Output() itemsPerPageChanged: EventEmitter<number> = new EventEmitter();
 
   public itemsPerPage: number[] = [
     20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200,
   ];
-  public selectedItemsPerPage: number = 30;
 
   get totalPages(): number {
-    return Math.ceil(this.totalItems / this.selectedItemsPerPage);
+    return this.totalItems > 0
+      ? Math.ceil(this.totalItems / this.selectedItemsPerPage)
+      : 1;
   }
 
   changePage(page: number): void {
@@ -29,5 +31,9 @@ export class PaginationComponent {
       this.currentPage = page;
       this.pageChanged.emit(page);
     }
+  }
+
+  onItemsPerPageChange(): void {
+    this.itemsPerPageChanged.emit(this.selectedItemsPerPage);
   }
 }

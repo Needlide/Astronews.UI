b644d3dff05c35c3362cb641330e2a9f645a8f72
Needlide <max.litvinyuck553@gmail.com>
2025-02-02 20:29:44 +0200

    feature: marked optional parameters, define method for pagination deletion, change signature for getSearch method marked ttl parameter as optional, refactor getSearch to include offset and itemsPerPage parameters, define deletePagination method and split clear method into clearPagination and clearSearch to manage search and pagination cache separately

--- src/app/cache/caching-service.interface.ts
+++ src/app/cache/caching-service.interface.ts
@@ -1,15 +1,24 @@
 export interface ICachingService<T> {
-  setPagination(page: number, value: T, ttl: number): void;
+  setPagination(page: number, value: T, ttl?: number): void;
 
-  setSearch(page: number, key: string, value: T, ttl: number): void;
+  setSearch(page: number, key: string, value: T, ttl?: number): void;
 
   getPagination(page: number): T | null;
 
-  getSearch(page: number, key: string): T | null;
+  getSearch(
+    page: number,
+    key: string,
+    offset: number,
+    itemsPerPage: number
+  ): T | null;
 
-  deletePagination(page: number): void; // deletes search tied to page too
+  // Delete the cached pagination and related search data
+  deletePagination(page: number): void;
 
+  // Delete the cached search data for a specific page and key
   deleteSearch(page: number, key: string): void;
 
-  clear(): void;
+  clearPagination(): void;
+
+  clearSearch(): void;
 }

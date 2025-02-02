a8efd658f8596966f6057cb2412ed49ef64eeb8e
Needlide <max.litvinyuck553@gmail.com>
2025-02-02 20:46:06 +0200

    refactor: update NewsCache model to include expiry and remove nextUrl and prevUrl Modified the NewsCache model to reflect new structure of cache: added expiry field and removed nextUrl/prevUrl fields

--- src/app/models/cache/news-cache.model.ts
+++ src/app/models/cache/news-cache.model.ts
@@ -2,7 +2,6 @@ import { NewsModel } from '../news/news.model';
 
 // model used to store the news data in cache
 export interface NewsCache {
-  nextUrl: string;
-  prevUrl: string;
   data: NewsModel[];
+  expiry: number;
 }

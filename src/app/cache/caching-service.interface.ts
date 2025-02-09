92d8b8809f6506be62d33dd2165a8cc28274f421
Needlide <max.litvinyuck553@gmail.com>
2025-02-09 20:55:15 +0200

    feature: mark `page` parameter as optional Made `page` parameter optional so the Mars Curiosity page can use the interface. Its search capabilities aren't tied to page, which makes it unused parameter

--- src/app/cache/caching-service.interface.ts
+++ src/app/cache/caching-service.interface.ts
@@ -1,17 +1,17 @@
 export interface ICachingService<T> {
   setPagination(page: number, value: T, ttl?: number): void;
 
-  setSearch(page: number, key: string, value: T, ttl?: number): void;
+  setSearch(key: string, value: T, page?: number, ttl?: number): void;
 
   getPagination(page: number): T | null;
 
-  getSearch(page: number, key: string): T | null;
+  getSearch(key: string, page?: number): T | null;
 
   // Delete the cached pagination and related search data
   deletePagination(page: number): void;
 
   // Delete the cached search data for a specific page and key
-  deleteSearch(page: number, key: string): void;
+  deleteSearch(key: string, page: number): void;
 
   clearPagination(): void;
 

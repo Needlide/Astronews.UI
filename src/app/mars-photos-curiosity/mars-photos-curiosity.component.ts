968442018f5209a0407645e3e014841e537c179a
Needlide <max.litvinyuck553@gmail.com>
2025-02-11 23:21:01 +0200

    refactor: use new state to handle pagination, data loading and search capabilities Added proper handling of observables. Implemented dispatching different actions as separate methods.

--- src/app/mars-photos-curiosity/mars-photos-curiosity.component.ts
+++ src/app/mars-photos-curiosity/mars-photos-curiosity.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MarsModel } from '../models/mars/mars.model';
-import { Observable } from 'rxjs';
+import { Observable, skip, Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { MarsCuriosityState } from './mars-photos-curiosity.reducer';
 import {
+  selectMarsCuriosityCurrentSol,
   selectMarsCuriosityData,
   selectMarsCuriosityError,
   selectMarsCuriosityIsLoading,
+  selectMarsCuriosityMaxDate,
+  selectMarsCuriosityMaxSol,
+  selectMarsCuriosityPaginationEnabled,
+  selectMarsCuriositySearchQuery,
+  selectMarsCuriosityTotalItems,
 } from './mars-photos-curiosity.selectors';
 import { MarsCuriosityActions } from './mars-photos-curiosity.actions';
 import { SearchService } from '../search/search.service';
@@ -16,7 +22,7 @@ import { SearchService } from '../search/search.service';
   templateUrl: './mars-photos-curiosity.component.html',
   styleUrls: ['./mars-photos-curiosity.component.scss'],
 })
-export class MarsPhotosCuriosityComponent implements OnInit {
+export class MarsPhotosCuriosityComponent implements OnInit, OnDestroy {
   data$: Observable<MarsModel[]> = this.store.select(selectMarsCuriosityData);
   isLoading$: Observable<boolean> = this.store.select(
     selectMarsCuriosityIsLoading
@@ -24,6 +30,30 @@ export class MarsPhotosCuriosityComponent implements OnInit {
   error$: Observable<string | null> = this.store.select(
     selectMarsCuriosityError
   );
+  currentSol$: Observable<number> = this.store.select(
+    selectMarsCuriosityCurrentSol
+  );
+  totalItems$: Observable<number> = this.store.select(
+    selectMarsCuriosityTotalItems
+  );
+  searchTerm$: Observable<string> = this.store.select(
+    selectMarsCuriositySearchQuery
+  );
+  maxSol$: Observable<number> = this.store.select(selectMarsCuriosityMaxSol);
+  maxDate$: Observable<string> = this.store.select(selectMarsCuriosityMaxDate);
+  paginationEnabled$: Observable<boolean> = this.store.select(
+    selectMarsCuriosityPaginationEnabled
+  );
+
+  private _subscriptions: Subscription[] = [];
+
+  error: string | null = null;
+  isLoading: boolean = false;
+  currentSol: number = 0;
+  maxSol: number = 0;
+  maxDate: string = '';
+  totalItems: number = 0;
+  paginationEnabled: boolean = true;
 
   constructor(
     private store: Store<MarsCuriosityState>,
@@ -31,10 +61,84 @@ export class MarsPhotosCuriosityComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.searchService.searchTerm$.subscribe((searchText) => {
-      this.store.dispatch(
-        MarsCuriosityActions.loadData({ searchTerm: searchText })
-      );
+    this.loadManifest();
+    this.setupSubscriptions();
+    this.trackSearchTerm();
+  }
+
+  private setupSubscriptions(): void {
+    const loadingSub = this.isLoading$.subscribe((isLoading) => {
+      this.isLoading = isLoading;
     });
+
+    const errorSub = this.error$.subscribe((error) => {
+      this.error = error;
+    });
+
+    const searchSub = this.searchService.searchTerm$.subscribe((searchText) => {
+      if (searchText) {
+        this.store.dispatch(
+          MarsCuriosityActions.setSearchQuery({ searchTerm: searchText })
+        );
+      } else if (searchText === '') {
+        this.store.dispatch(
+          MarsCuriosityActions.setSearchQuery({ searchTerm: '' })
+        );
+      }
+    });
+
+    const currentSolSub = this.currentSol$.subscribe((currentSol) => {
+      this.currentSol = currentSol;
+    });
+
+    const totalItemsSub = this.totalItems$.subscribe((totalItems) => {
+      this.totalItems = totalItems;
+    });
+
+    const maxSolSub = this.maxSol$.subscribe((maxSol) => {
+      this.maxSol = maxSol;
+    });
+
+    const paginationEnabledSub = this.paginationEnabled$.subscribe(
+      (paginationEnabled) => {
+        this.paginationEnabled = paginationEnabled;
+      }
+    );
+
+    this._subscriptions.push(
+      loadingSub,
+      errorSub,
+      searchSub,
+      currentSolSub,
+      totalItemsSub,
+      maxSolSub,
+      paginationEnabledSub
+    );
+  }
+
+  private loadManifest(): void {
+    this.store.dispatch(MarsCuriosityActions.loadManifest());
+  }
+
+  private trackSearchTerm(): void {
+    const searchTermSub = this.searchTerm$
+      .pipe(skip(1))
+      .subscribe((searchText) => {
+        if (searchText) {
+          this.store.dispatch(MarsCuriosityActions.initiateSearch());
+        } else if (searchText === '') {
+          this.store.dispatch(MarsCuriosityActions.loadData());
+        }
+      });
+
+    this._subscriptions.push(searchTermSub);
+  }
+
+  onPageChanged(page: number): void {
+    this.store.dispatch(MarsCuriosityActions.setSol({ currentSol: page }));
+  }
+
+  ngOnDestroy(): void {
+    this._subscriptions.forEach((sub) => sub.unsubscribe());
   }
 }

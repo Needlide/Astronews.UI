4b70f8baad823a50690fd2b0ca2b5567ff814156
Needlide <max.litvinyuck553@gmail.com>
2025-02-11 23:12:47 +0200

    feat: add effects to handle loading initial data about the rover, pagination, searching Implemented effects for fetching and caching data of rover's manifest to retrieve the initial data (total items, max available sol), load initial photo data, pagination and search data

--- src/app/mars-photos-curiosity/mars-photos-curiosity.effects.ts
+++ src/app/mars-photos-curiosity/mars-photos-curiosity.effects.ts
@@ -1,24 +1,83 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { MarsCuriosityActions } from './mars-photos-curiosity.actions';
-import { catchError, map, of, switchMap } from 'rxjs';
+import {
+  catchError,
+  concatMap,
+  map,
+  mergeMap,
+  of,
+  switchMap,
+  withLatestFrom,
+} from 'rxjs';
 import { Injectable } from '@angular/core';
 import { MarsCuriositySearchService } from '../search/mars-curiosity-search.service';
+import { Store } from '@ngrx/store';
+import {
+  selectMarsCuriosityCurrentSol,
+  selectMarsCuriositySearchQuery,
+} from './mars-photos-curiosity.selectors';
 
 @Injectable()
 export class MarsCuriosityEffects {
   constructor(
     private actions$: Actions,
-    private searchService: MarsCuriositySearchService
+    private searchService: MarsCuriositySearchService,
+    private readonly store: Store
   ) {}
+
+  loadManifest$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(MarsCuriosityActions.loadManifest),
+      switchMap(() =>
+        this.searchService.getManifest().pipe(
+          map((data) => {
+            this.store.dispatch(
+              MarsCuriosityActions.loadManifestSuccess({
+                data,
+              })
+            );
+          }),
+          catchError((error) =>
+            of(MarsCuriosityActions.loadManifestFailure({ error }))
+          )
+        )
+      ),
+      concatMap(() => of(MarsCuriosityActions.loadData()))
+    )
+  );
 
   loadData$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(MarsCuriosityActions.loadData),
-      switchMap((action) =>
-        this.searchService.search(action.searchTerm).pipe(
-          map((data) => MarsCuriosityActions.loadDataSuccess({ data })),
+      ofType(MarsCuriosityActions.loadData, MarsCuriosityActions.setSol),
+      switchMap(() =>
+        this.store.select(selectMarsCuriosityCurrentSol).pipe(
+          switchMap((currentSol) =>
+            this.searchService.load(currentSol).pipe(
+              map((data) => {
+                return MarsCuriosityActions.loadDataSuccess({ data });
+              }),
+              catchError((error) =>
+                of(MarsCuriosityActions.loadDataFailure({ error }))
+              )
+            )
+          )
+        )
+      )
+    )
+  );
+
+  initiateSearch$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(MarsCuriosityActions.initiateSearch),
+      withLatestFrom(
+        this.store.select(selectMarsCuriositySearchQuery),
+        this.store.select(selectMarsCuriosityCurrentSol)
+      ),
+      mergeMap(([, searchQuery, currentSol]) =>
+        this.searchService.search(currentSol, searchQuery).pipe(
+          map((data) => MarsCuriosityActions.initiateSearchSuccess({ data })),
           catchError((error) =>
-            of(MarsCuriosityActions.loadDataFailure({ error }))
+            of(MarsCuriosityActions.initiateSearchFailure({ error }))
           )
         )
       )

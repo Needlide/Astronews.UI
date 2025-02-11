bc3a8f1660a201b4c328903a692b66dfea67f16a
Needlide <max.litvinyuck553@gmail.com>
2025-02-11 18:47:34 +0200

    refactor: reflect changes in the interface, remove `page` parameter from search related methods, simplify search cache by removing parental map

--- src/app/cache/mars-curiosity/caching-curiosity.service.ts
+++ src/app/cache/mars-curiosity/caching-curiosity.service.ts
@@ -8,8 +8,7 @@ import { MarsCuriosityCache } from '@/app/models/cache/mars-curiosity.model';
 })
 export class CachingCuriosityService implements ICachingService<MarsModel[]> {
   private _paginationCache: Map<number, MarsCuriosityCache> = new Map();
-  private _searchCache: Map<number, Map<string, MarsCuriosityCache>> =
-    new Map();
+  private _searchCache: Map<string, MarsCuriosityCache> = new Map();
 
   // 21_600_000 = 6 hours
   private readonly _ttl = 21_600_000;
@@ -26,28 +25,19 @@ export class CachingCuriosityService implements ICachingService<MarsModel[]> {
     this._paginationCache.set(page, { data: value, expiry });
   }
 
-  setSearch(
-    page: number,
-    key: string,
-    value: MarsModel[],
-    ttl: number = this._ttl
-  ): void {
-    const pageCache =
-      this._searchCache.get(page) || new Map<string, MarsCuriosityCache>();
+  setSearch(key: string, value: MarsModel[], ttl: number = this._ttl): void {
     const expiry = Date.now() + ttl;
 
-    pageCache.set(key.toLowerCase(), { data: value, expiry });
-    this._searchCache.set(page, pageCache);
+    this._searchCache.set(key, { data: value, expiry });
   }
 
   getPagination(page: number): MarsModel[] | null {
     return this.getValidCache(this._paginationCache.get(page));
   }
 
-  getSearch(page: number, key: string): MarsModel[] | null {
-    const pageCache = this._searchCache.get(page);
+  getSearch(key: string): MarsModel[] | null {
+    const cache = this._searchCache.get(key);
 
-    const cache = pageCache ? pageCache.get(key.toLowerCase()) : undefined;
     return this.getValidCache(cache);
   }
 
@@ -55,18 +45,8 @@ export class CachingCuriosityService implements ICachingService<MarsModel[]> {
     this._paginationCache.delete(page);
   }
 
-  deleteSearch(page: number, key: string): void {
-    const pageCache = this._searchCache.get(page);
-
-    if (pageCache) {
-      pageCache.delete(key.toLowerCase());
-
-      if (pageCache.size === 0) {
-        this._searchCache.delete(page);
-      } else {
-        this._searchCache.set(page, pageCache);
-      }
-    }
+  deleteSearch(key: string): void {
+    this._searchCache.delete(key);
   }
 
   clearPagination(): void {

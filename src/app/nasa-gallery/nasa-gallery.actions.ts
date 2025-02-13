1b020fd11b0ea4460675e6e970f9a5b69965f97a
Needlide <max.litvinyuck553@gmail.com>
2025-02-13 23:47:50 +0200

    refactor: add new actions for pagination, search capabilities Added new actions as in News page

--- src/app/nasa-gallery/nasa-gallery.actions.ts
+++ src/app/nasa-gallery/nasa-gallery.actions.ts
@@ -1,11 +1,26 @@
-import { createActionGroup, props } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 import { Data } from '../models/gallery/gallery.root.model';
 
 export const NasaGalleryActions = createActionGroup({
   source: 'Nasa Gallery Page',
   events: {
-    'Load Data': props<{ searchTerm: string }>(),
+    'Load Data': emptyProps(),
     'Load Data Success': props<{ data: Data[] }>(),
     'Load Data Failure': props<{ error: string }>(),
+
+    'Initiate Search': emptyProps(),
+    'Initiate Search Success': props<{ data: Data[] }>(),
+    'Initiate Search Failure': props<{ error: string }>(),
+
+    'Set Page': props<{ currentPage: number }>(),
+    'Set Items Per Page': props<{ itemsPerPage: number }>(),
+    'Set Total Items': props<{ totalItems: number }>(),
+
+    'Set Search Page': props<{ searchPage: number }>(),
+    'Set Search Items Per Page': props<{ searchItemsPerPage: number }>(),
+    'Set Total Search Items': props<{ totalSearchItems: number }>(),
+
+    'Set Error': props<{ error: string }>(),
+    'Set Search Query': props<{ searchTerm: string }>(),
   },
 });

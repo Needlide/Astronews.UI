f986d53eef98f6472fa6e9abdd22582bcd28cdf9
Needlide <max.litvinyuck553@gmail.com>
2025-02-13 23:50:02 +0200

    refactor: add selectors for new parameters of the state, sort by states

--- src/app/nasa-gallery/nasa-gallery.selectors.ts
+++ src/app/nasa-gallery/nasa-gallery.selectors.ts
@@ -4,12 +4,55 @@ import { NasaGalleryState } from './nasa-gallery.reducer';
 export const selectNasaGalleryState =
   createFeatureSelector<NasaGalleryState>('nasaGallery');
 
+// Selectors for pagination
+export const selectNasaGalleryPage = createSelector(
+  selectNasaGalleryState,
+  (state) => state.page
+);
+
+export const selectNasaGalleryTotalItems = createSelector(
+  selectNasaGalleryState,
+  (state) => state.totalItems
+);
+
+export const selectNasaGalleryItemsPerPage = createSelector(
+  selectNasaGalleryState,
+  (state) => state.itemsPerPage
+);
+
+// Selectors for search state
+export const selectNasaGallerySearchState = createSelector(
+  selectNasaGalleryState,
+  (state) => state.searchState
+);
+
+export const selectNasaGallerySearchPage = createSelector(
+  selectNasaGallerySearchState,
+  (searchState) => searchState.page
+);
+
+export const selectNasaGalleryTotalSearchItems = createSelector(
+  selectNasaGallerySearchState,
+  (searchState) => searchState.totalItems
+);
+
+export const selectNasaGallerySearchItemsPerPage = createSelector(
+  selectNasaGallerySearchState,
+  (searchState) => searchState.itemsPerPage
+);
+
+export const selectNasaGallerySearchQuery = createSelector(
+  selectNasaGallerySearchState,
+  (searchState) => searchState.searchQuery
+);
+
+// General selectors
 export const selectNasaGalleryData = createSelector(
   selectNasaGalleryState,
   (state) => state.data
 );
 
-export const selectNasaGalleryIsLoading = createSelector(
+export const selectNasaGalleryLoading = createSelector(
   selectNasaGalleryState,
   (state) => state.isLoading
 );
@@ -17,4 +60,9 @@ export const selectNasaGalleryIsLoading = createSelector(
 export const selectNasaGalleryError = createSelector(
   selectNasaGalleryState,
   (state) => state.error
+);
+
+export const selectNasaGalleryIsSearchState = createSelector(
+  selectNasaGalleryState,
+  (state) => state.isSearchState
 );
